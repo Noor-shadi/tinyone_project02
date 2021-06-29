9b3f7ca3c62369cf235c55d3d7d080c4dc169f4e
Noor-Shadi <85039502+Noor-shadi@users.noreply.github.com>
2021-06-29 23:27:30 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// banner part start==========
+
+$('.banner-slider').slick({
+    dots: true,
+    arrows:false,
+    infinite: false,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+
+  // banner part end============
